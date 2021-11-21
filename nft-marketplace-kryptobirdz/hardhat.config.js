ea58c566726300c630ebeb25c6e23a6c277669d2
Kristjan Bajuk <kristjan.bajuk@gmail.com>
2021-11-21 17:40:36 +0100

    add contrats

--- nft-marketplace-kryptobirdz/hardhat.config.js
+++ nft-marketplace-kryptobirdz/hardhat.config.js
@@ -1,21 +1,32 @@
 require("@nomiclabs/hardhat-waffle");
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+const projectId = '85e946043c9f47fda6f5b2b9c1fcb911';
+const fs = require('fs');
+const keyData = fs.readFileSync('./p-key.txt', {
+  encoding: 'utf-8', flag: 'r'
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 1337 // config standard
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [keyData]
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${projectId}`,
+      accounts:[keyData]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      } 
+    }
+  },
 };
